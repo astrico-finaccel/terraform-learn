bc0ea3e7018879fec4f3d26cc068823437e07c0e
astrico <astrico@astricos-MacBook-Pro.local>
2021-08-05 13:12:55 +0700

    add files

---
+++ vm/main.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  region  = "ap-southeast-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+variable "subnet_id" {
+  type = string
+}
+variable "security_group_id" {
+  type = list(string)
+}
+
+resource "aws_network_interface" "ast-ni" {
+  subnet_id   = var.subnet_id
+  security_groups = var.security_group_id
+
+  tags = {
+    Name = "ast-ni"
+  }
+}
+
+variable "public_key" {
+  type = string
+}
+resource "aws_instance" "ast-vm" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name = var.public_key
+
+  network_interface {
+    network_interface_id = aws_network_interface.ast-ni.id
+    device_index         = 0
+  }
+
+  tags = {
+    Name = "ast-vm"
+  }
+}
+
---
+++ vpc/main.tf
@@ -0,0 +1,129 @@
+provider "aws" {
+  region  = "ap-southeast-1"
+}
+
+resource "aws_vpc" "ast-test" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "ast-test"
+  }
+}
+output "vpc_id" {
+  value = aws_vpc.ast-test.id
+}
+
+resource "aws_subnet" "ast-public" {
+  vpc_id                  = aws_vpc.ast-test.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "ast-public"
+  }
+}
+output "public_subnet_id"{
+  value = aws_subnet.ast-public.id
+}
+
+resource "aws_subnet" "ast-private" {
+  vpc_id     = aws_vpc.ast-test.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "ast-private"
+  }
+}
+output "private_subnet_id" {
+  value = aws_subnet.ast-private.id
+}
+
+resource "aws_internet_gateway" "ast-igw" {
+  vpc_id = aws_vpc.ast-test.id
+
+  tags = {
+    Name = "ast-igw"
+  }
+}
+
+resource "aws_eip" "ast-eip" {
+  vpc      = true
+  tags = {
+    Name = "ast-eip"
+  }
+}
+
+resource "aws_nat_gateway" "ast-nat" {
+  allocation_id = aws_eip.ast-eip.id
+  subnet_id     = aws_subnet.ast-public.id
+
+  tags = {
+    Name = "ast-nat"
+  }
+
+  depends_on = [aws_internet_gateway.ast-igw, aws_eip.ast-eip]
+}
+
+resource "aws_route_table" "ast-rt-pub" {
+  vpc_id = aws_vpc.ast-test.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ast-igw.id
+  }
+}
+
+resource "aws_route_table" "ast-rt-priv" {
+  vpc_id = aws_vpc.ast-test.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.ast-nat.id
+  }
+}
+
+resource "aws_route_table_association" "ast-pub-rta" {
+  subnet_id      = aws_subnet.ast-public.id
+  route_table_id = aws_route_table.ast-rt-pub.id
+}
+
+resource "aws_route_table_association" "ast-priv-rta" {
+  subnet_id      = aws_subnet.ast-private.id
+  route_table_id = aws_route_table.ast-rt-priv.id
+}
+
+resource "aws_security_group" "ast-allow-default" {
+  name        = "ast-allow-default"
+  description = "Allow TLS & SSH inbound traffic"
+  vpc_id      = aws_vpc.ast-test.id
+
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ast-allow-default"
+  }
+}
+output "security_group_id"{
+  value = aws_security_group.ast-allow-default.id
+}
